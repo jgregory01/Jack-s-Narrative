a01718862cd573c69344402bf1e06ba190186087
Jack Tea Gregory <jgregory@risd.edu>
2014-05-19 23:02:16 -0400

    narrative

---
+++ _scripts/global.js
@@ -0,0 +1,71 @@
+function load(){
+confirm("CHOOSE YOUR CHILD'S DESTINY BEFORE THE GESTATION PERIOD IS COMPLETE !!!");
+setInterval(function()
+{ for(var i=1; i>=1; i++)console.log(i);},272000);}
+
+$(document).ready(function(){
+
+	//register event handlers
+	
+	$('#vid1').click(function() {
+		alert("Congratulations, you've chosen correctly before it was too late!");
+
+	});
+	$('#vid2').click(function() {
+	
+		alert("Congratulations, you've chosen correctly before it was too late!");
+		
+	});
+	$('#vid3').click(function() {
+	
+		alert("Congratulations, you've chosen correctly before it was too late!x");
+		
+	});
+	$('#vid4').click(function() {
+	
+		alert("Congratulations, you've chosen correctly before it was too late!");
+		
+	});
+	$('#vid5').click(function() {
+	
+		alert("Congratulations, you've chosen correctly before it was too late!");
+		
+	});
+	$('#vid6').click(function() {
+	
+		alert("Congratulations, you've chosen correctly before it was too late!");
+		
+	});
+});
+/*
+var x;
+var box=new 
+switch(box) {
+case 1:
+	x= {$("vid2", "vid3", "vid4", "vid5", "vid6").player.setVolume(0);};
+	break;
+case 2:
+x= {$("vid1", "vid3", "vid4", "vid5", "vid6").player.setVolume(0);};
+	break;
+*/
+
+
+//Every 3 minutes, after the video ends, the page will crash
+
+//when clicked, video should expand to full screen
+// as video expands, all other sound except video's noise goes away
+// if video completes before baby is completed an alert should pop up and say "congratulations you've chosen correctly before time ran out!"
+
+//set up a transition similar to the opacity where z-index gets changed. (won't work--> why?)
+
+//if (statement) video is more than X px (the scale transition has already occured,) begin timer . 
+//Once timer reaches (switch station depending on which video was clicked), set up alert
+
+/* var onClickB = function () {
+if {getElementByName(".box") === 0 ) {
+  console.log("The statement is true");
+} else {
+  console.log("The statement is false");
+}
+*/
+
